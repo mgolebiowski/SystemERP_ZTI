0d654a99f939c73c2cc4f97cd89e73f1677ba471
mgol <michal.golabek94@gmail.com>
2017-03-05 09:50:25 +0100

    init

---
+++ ERP_ZTI/ERP_ZTI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ERP_ZTI.MvcApplication" Language="C#" %>
---
+++ ERP_ZTI/ERP_ZTI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERP_ZTI.Startup))]
+namespace ERP_ZTI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
