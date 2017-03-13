feaf92be2b116f5f341fb415ea7fbfcfdcd3b23a
mgol <michal.golabek94@gmail.com>
2017-03-13 22:21:57 +0100

    Added controllers and views of Production, Orders, Storage and Sales. Updated frontend.

---
+++ ERP_ZTI/ERP_ZTI/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP_ZTI
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ERP_ZTI/ERP_ZTI/Controllers/StorageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP_ZTI.Controllers
+{
+    public class StorageController : Controller
+    {
+        // GET: Storage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
