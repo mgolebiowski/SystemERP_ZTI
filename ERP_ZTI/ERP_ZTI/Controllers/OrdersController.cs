dea1701c3c987e55f245f5235b1a1383908d2aba
mgol <michal.golabek94@gmail.com>
2017-05-25 10:58:26 +0200

    Added model resposible for notifications for orders dep and overhauled the orders view

--- ERP_ZTI/ERP_ZTI/Controllers/OrdersController.cs
+++ ERP_ZTI/ERP_ZTI/Controllers/OrdersController.cs
@@ -8,10 +8,33 @@ namespace ERP_ZTI
 {
     public class OrdersController : Controller
     {
-        // GET: Orders
+        Models.ERP_DBEntities db = new Models.ERP_DBEntities();
+
+        // GET Orders
         public ActionResult Index()
         {
+            var notifications = new Models.NotificationModel();
+            ViewBag.zeroA = notifications.zeroAmount;
+            ViewBag.smallA = notifications.smallAmount;
             return View();
+        }
+
+        // GET: Orders/Orders
+        public ActionResult Orders()
+        {
+            var orders = from o in db.Orders
+                        select o;
+
+            return View(orders.AsEnumerable());
+        }
+
+        // GET: Contractors
+        public ActionResult Contractors()
+        {
+            var contr = from c in db.Contractors
+                         select c;
+
+            return View(contr.AsEnumerable());
         }
     }
 }
---
+++ ERP_ZTI/ERP_ZTI/Models/NotificationModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERP_ZTI.Models
+{
+    public class NotificationModel
+    {
+        public List<Models.Products> smallAmount { get; set; }
+        public List<Models.Products> zeroAmount { get; set; }
+
+        //Constructor
+        public NotificationModel()
+        {
+            //Instantiate the lists
+            this.smallAmount = new List<Products>();
+            this.zeroAmount = new List<Products>();
+            //Get amounts of all products
+            Models.ERP_DBEntities db = new Models.ERP_DBEntities();
+            var products = from p in db.Products
+                          select p;
+            //Fulfill the propeties of class  
+            foreach(Products product in products)
+            {
+                int amount;
+                int.TryParse(product.Amount,out amount);
+                if (amount == 0)
+                {
+                    this.zeroAmount.Add(product);
+                    
+                }
+                else if(amount < 5)
+                {
+                    this.smallAmount.Add(product);
+                }
+            }
+        }
+    }
+}
